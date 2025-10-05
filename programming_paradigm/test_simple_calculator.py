37ef2382aff1b067b24021cd76b15b40835a4631
Mugisha Joshua <mugishajoshua567@gmail.com>
2025-10-05 12:46:32 +0200

    new commit

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,7 +13,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-5, -7), -12)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test subtraction operation."""
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(0, 5), -5)
